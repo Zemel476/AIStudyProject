075c61ecbd8440b99cb8018f810af62c4d922ea5
Zemel <shuiwy_333@163.com>
2025-12-08 21:09:34 +0800

    add file

---
+++ BasicPython/day01/add.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/12/8 21:04
+# @Author  : 老冰棍
+# @File    : add.py
+# @Software: PyCharm
---
+++ BasicPython/day01/main.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/12/8 21:06
+# @Author  : 老冰棍
+# @File    : main.py
+# @Software: PyCharm
